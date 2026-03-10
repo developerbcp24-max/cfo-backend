71e47737a09182989a3cb8e652b01bddd6ab7f23
developerbcp24-max <developerbcp24@gmail.com>
2026-03-10 12:28:47 -0400

    crear carpeta uploads si no existe

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,16 @@ import express from "express";
 import cors from "cors";
 import * as router from "./routes/index";
 import path from "path";
+import fs from "fs";
 
 const app = express();
 const PORT = 3000;
+
+// Crear carpeta uploads si no existe
+const uploadDir = path.join(__dirname, "../uploads");
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
 
 app.use(cors());
 app.use(express.json());
@@ -14,7 +21,7 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../index.html"));
 });
 
-app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+app.use("/uploads", express.static(uploadDir));
 
 // Rutas
 app.use("/report", router.routerReport);
